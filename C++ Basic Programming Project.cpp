51224967b60b535ceaa03d92688c5a639fa6c575
Israr <israr.akh03@gmail.com>
2024-09-14 10:35:23 -0400

    Add project files.

---
+++ C++ Basic Programming Project.cpp
@@ -0,0 +1,10 @@
+// C++ Basic Programming Project.cpp : This project covers examples Distance per Tank of Gas and
+// Energy Drink Consumption.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
