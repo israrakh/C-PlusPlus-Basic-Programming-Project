27b557dbe171c59f557a94a691daba96af3be6a4
Israr <israr.akh03@gmail.com>
2024-09-14 12:13:33 -0400

    Energy Drink Consumption

--- C++ Basic Programming Project.cpp
+++ C++ Basic Programming Project.cpp
@@ -1,11 +1,31 @@
 // C++ Basic Programming Project.cpp : This project covers examples Distance per Tank of Gas and
 // Energy Drink Consumption.
 
+
+// Energy Drink Consumption programming example.
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "This project covers examples Distance per Tank of Gas and " <<
-        " Energy Drink Consumption.\n";
+	// Declaring and initializing variables.
+	int customers = 16500;
+	// Percentage of customers purchasing energy drinks 15% = 0.15
+	double energyDrinkCustomersPercentage = 0.15;
+	// percentage of energy drink customers preferring citrus flavor 58% = 0.58
+	double citrusDrinkCustomersPercentage = 0.58;
+
+	// Calculating the approximate number of customers in the survey purchasing one or more energy drinks per week.
+	int energyDrinkCustomers = customers * energyDrinkCustomersPercentage;
+	// Calculating the approximate number of customers in the survey preferring citrus-flavored energy drinks.
+	int citrusDrinkCustomers = energyDrinkCustomers * citrusDrinkCustomersPercentage;
+
+	// Displaying the result
+	cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week are: "
+		<< energyDrinkCustomers << endl;
+	cout << "The approximate number of customers in the survey preferring citrus-flavored energy drinks are: "
+		<< citrusDrinkCustomers << endl;
+
+	return 0;
 }
 
