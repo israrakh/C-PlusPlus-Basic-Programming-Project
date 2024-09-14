40edd531256e80f728d17435adbd93e5073f913c
Israr Akhundzada <133725332+israrakh@users.noreply.github.com>
2024-09-14 12:33:51 -0400

    Update C++ Basic Programming Project.cpp

--- C++ Basic Programming Project.cpp
+++ C++ Basic Programming Project.cpp
@@ -24,26 +24,7 @@ int main()
 		<< energyDrinkCustomers << endl;
 	cout << "The approximate number of customers in the survey preferring citrus-flavored energy drinks are: "
 		<< citrusDrinkCustomers << endl;
-
-
-// Distance per Tank of Gas programming example.
-#include <iostream>
-using namespace std;
-
-// Initializing constant variables
-const double TANK_SIZE = 20.0; // gallons
-const double MILES_PER_GALLON_TOWN = 23.5; // miles per gallon in town
-const double MILES_PER_GALLON_HIGHWAY = 28.9; // miles per gallon on highway
-
-int main()
-{
-	// calculating distances for town and highway
-	double distanceTown = TANK_SIZE * MILES_PER_GALLON_TOWN;
-	double distanceHighway = TANK_SIZE * MILES_PER_GALLON_HIGHWAY;
 	
-	// Displaying results.
-	cout << "The car can travel " << distanceTown << " miles on a tank of gas in town." << endl;
-	cout << "The car can travel " << distanceHighway << " miles on a tank of gas on the highway." << endl;
 	return 0;
 }
 
